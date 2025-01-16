f5a3cb0e06306c44e6bcc6148bb7b127f2e4905a
diogosantosgoncalves <diogo_santos_goncalves@hotmail.com>
2025-01-16 07:33:40 -0300

    criando configurações de url, rotas e interface pessoa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Pessoa } from './models/pessoa';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,18 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'TodoListAngular';
+  http = inject(HttpClient);
+  urlApi = 'http://localhost:7233';
+  pessoas: Pessoa[] = [];
+  
+  ngOnInit(): void {
+    this.obterPessoas();
+  }
+
+  obterPessoas() {
+    this.http.get<Pessoa[]>(`${this.urlApi}/pessoas`)
+      .subscribe(pessoas => this.pessoas = pessoas)
+  }
 }
